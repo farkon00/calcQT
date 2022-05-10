a637fe7e0ee8070f787f8f7d7f979ae03bdea132
farkon00 <sammer2016sammer@gmail.com>
2022-05-10 11:51:12 +0300

    Text and better layout added

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 class MainWindow(QMainWindow):
+    BUTTONS_TEXTS = [
+        ["MC", "MR", "MS", "M+", "M-"],
+        ["⬅", "CE", "C",  "±",  "√"],
+        ["7",  "8",  "9",  "/",  "%"],
+        ["4",  "5",  "6",  "*",  "1/x"],
+        ["1",  "2",  "3",  "-",  "="],
+        ["0",  "",   ".",  "+",  ""],
+    ]
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
@@ -14,9 +23,14 @@ class MainWindow(QMainWindow):
         self.main = QVBoxLayout()
         self.buttons = QGridLayout()
 
-        for i in range(5):
+        for i in range(6):
             for j in range(5):
-                self.buttons.addWidget(QPushButton(), i, j)
+                if (i, j) in ((5, 1), (5, 4)): # Cells where second half of two-cell buttons lies and empty cells
+                    continue
+                elif (i, j) == (5, 0): # 0
+                    self.buttons.addWidget(QPushButton(MainWindow.BUTTONS_TEXTS[i][j]), i, j, 1, 2)
+                else:
+                    self.buttons.addWidget(QPushButton(MainWindow.BUTTONS_TEXTS[i][j]), i, j)
 
         self.inp = QLineEdit()
         self.inp.setMinimumHeight(50)
